9e473e0ebbb229901d42ee94c44e7c122b651373
Xuni Huang <xunih@outlook.com>
2022-04-07 12:01:29 +0200

    Set up a simple home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSmile } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -13,4 +16,8 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    library.add(faSmile);
+  }
+}
